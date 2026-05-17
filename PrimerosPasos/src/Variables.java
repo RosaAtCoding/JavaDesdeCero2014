b70676303daecbcbea9148808612f542396226c5
RosaAtCoding <rosaatcoding@gmail.com>
2026-05-17 20:59:10 +0200

    Video 6. Estructuras principales III. Declaración variables. Eclipse

---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+
+	public static void main(String[] args) {
+
+//		byte edad;
+
+//		short edad;
+
+//		int edad;
+		
+//		edad = 35;
+		
+		int edad = 35;	// Declaración e iniciación de variable en una única línea
+		
+		/* La siguiente instrucción imprime en la consola (System.out) 
+		 * el valor de la variable 'edad'
+		 */
+		System.out.println(edad);
+		
+		edad = 75;
+
+		System.out.println(edad);
+	}
+}
